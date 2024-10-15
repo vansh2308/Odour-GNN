5ace920ce9f8ae3b78cb9fb009562a4b205a42ee
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-10-16 02:30:15 +0530

    Create pytorch graph from molecule

--- dataset.py
+++ dataset.py
@@ -35,7 +35,7 @@ from torch_geometric.data import Data
 from torch.utils.data import DataLoader
 
 
-def get_atom_features(atom, use_chirality = False,  hydrogens_implicit = False) -> np.array: 
+def get_atom_features(atom, use_chirality = True,  hydrogens_implicit = True) -> np.array: 
     '''
     Takes an RDKit atom object as input and gives a 1d-numpy array of atom features as output.
     '''
@@ -92,12 +92,83 @@ def get_bond_features(bond,  use_stereochemistry = True):
     return np.array(bond_feature_vector)
 
 
+
+
+def create_pytorch_geometric_graph_list(x_smiles, y):
+    '''
+    Inputs:
+    
+    x_smiles = [smiles_1, smiles_2, ....] ... a list of SMILES strings
+    y = [y_1, y_2, ...] ... a list of numerial labels for the SMILES strings (such as associated pKi values)
+    
+    Outputs:
+    
+    data_list = [G_1, G_2, ...] ... a list of torch_geometric.data.Data objects which represent labeled molecular graphs that can readily be used for machine learning
+    '''
+        
+    data_list = []
+    
+    for (smiles, y_val) in zip(x_smiles, y):
+        
+        # convert SMILES to RDKit mol object
+        mol = Chem.MolFromSmiles(smiles)
+
+        # get feature dimensions
+        n_nodes = mol.GetNumAtoms()
+        n_edges = 2*mol.GetNumBonds()
+        unrelated_smiles = "O=O"
+        unrelated_mol = Chem.MolFromSmiles(unrelated_smiles)
+        n_node_features = len(get_atom_features(unrelated_mol.GetAtomWithIdx(0)))
+        n_edge_features = len(get_bond_features(unrelated_mol.GetBondBetweenAtoms(0,1)))
+
+        # construct node feature matrix X of shape (n_nodes, n_node_features)
+        X = np.zeros((n_nodes, n_node_features))
+
+        for atom in mol.GetAtoms():
+            X[atom.GetIdx(), :] = get_atom_features(atom)
+            
+        X = torch.tensor(X, dtype = torch.float)
+        
+        # construct edge index array E of shape (2, n_edges)
+        (rows, cols) = np.nonzero(GetAdjacencyMatrix(mol))
+        torch_rows = torch.from_numpy(rows.astype(np.int64)).to(torch.long)
+        torch_cols = torch.from_numpy(cols.astype(np.int64)).to(torch.long)
+        E = torch.stack([torch_rows, torch_cols], dim = 0)
+        
+        # construct edge feature array EF of shape (n_edges, n_edge_features)
+        EF = np.zeros((n_edges, n_edge_features))
+        
+        for (k, (i,j)) in enumerate(zip(rows, cols)):
+            
+            EF[k] = get_bond_features(mol.GetBondBetweenAtoms(int(i),int(j)))
+        
+        EF = torch.tensor(EF, dtype = torch.float)
+        
+        # construct label tensor
+        y_tensor = torch.tensor(y_val, dtype = torch.float)
+        
+        # construct Pytorch Geometric data object and append to data list
+        data_list.append(Data(x = X, edge_index = E, edge_attr = EF, y = y_tensor))
+
+    return data_list
+
+
 if __name__ == '__main__':
-    df_path = 'data/Multi-Labelled_Smiles_Odors_dataset.csv'
-    odor_df = pd.read_csv(df_path)
+    pass
 
-    m = Chem.MolFromSmiles(odor_df['nonStereoSMILES'][5])
-    atom_features = get_atom_features(m.GetAtoms()[0], True, True)
-    bond_feature = get_bond_features(m.GetBonds()[0], True)
+    # df_path = 'data/Multi-Labelled_Smiles_Odors_dataset.csv'
+    # odor_df = pd.read_csv(df_path)
 
-    print(bond_feature)
+    # m = Chem.MolFromSmiles(odor_df['nonStereoSMILES'][5])
+    # # atom_features = get_atom_features(m.GetAtoms()[0], True, True)
+    # # bond_feature = get_bond_features(m.GetBonds()[0], True)
+
+    # # print(bond_feature)
+
+    # X = odor_df.iloc[:, 0]
+    # classes = odor_df.columns[2:]
+    # y = odor_df.iloc[:, 2:].values
+
+
+    # graph_list = create_pytorch_geometric_graph_list(X, y)
+    # # print(len(graph_list))
