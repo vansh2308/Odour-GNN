76741efeca8fd9a3b2b69e4f319eaae0cdf0e4ac
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-10-17 02:33:08 +0530

    Add plot utils

--- utils.py
+++ utils.py
@@ -1,7 +1,19 @@
 
+import matplotlib.pyplot as plt 
+
 
 def label_encoding(x, domain:list):
     return [len(domain)-1] if x not in domain else [domain.index(x) ]
 
+def plot_losses(train_losses, val_losses, title=None):
+    """Utility method to plot paired loss curves"""
+    plt.plot(range(len(val_losses)), val_losses, label='val')
+    plt.plot(range(len(train_losses)), train_losses, label='train')
+    plt.legend()
+    plt.title(title)
+    plt.show()
+
+
 if __name__ == '__main__':
     pass
+
